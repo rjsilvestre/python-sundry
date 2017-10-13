644ea894e575d8ce01cf90dd78d4052361db1579
Rafael José <rjsilvestre@gmail.com>
2017-10-13 19:56:47 +0800

    Added edx mitx 6.00.1x prob set 1 ex 3 - find the longest alphabetically sorted sub-string

---
+++ edx-mitx-6.00.1x-pset1_3-sorted_alphabet.py
@@ -0,0 +1,24 @@
+def sort_alphabet(s):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    i = 0
+    j = 0
+    sort = ''
+    cur_sort = ''
+    
+    while i < len(s):
+        while j < len(alphabet):
+            if s[i] == alphabet[j]:
+                cur_sort += s[i]
+                if len(cur_sort) > len(sort):
+                    sort = cur_sort
+                break
+            j += 1
+        if j == len(alphabet):
+            j = 0
+            cur_sort = ''
+        else:
+            i += 1
+    return sort
+
+# Test case
+print(sort_alphabet('azcbobobegghakl'))
