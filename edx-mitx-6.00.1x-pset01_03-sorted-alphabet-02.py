691084e6ff0ad5b2b757ec56b1684825922f4e16
Rafael José <rjsilvestre@gmail.com>
2017-10-17 14:02:42 +0800

    Renaming part 2

---
+++ edx-mitx-6.00.1x-pset01_03-sorted-alphabet-02.py
@@ -0,0 +1,15 @@
+def sort_alphabet(s):
+    sort = s[0]
+    cur_sort = s[0]
+    for i in range(len(s) - 1):
+        if s[i + 1] >= s[i]: 
+            cur_sort += s[i + 1]
+            if len(cur_sort) > len(sort):
+                sort = cur_sort
+        else:
+            cur_sort = s[i + 1]
+    return sort
+
+# Test case
+string = 'azcbobobegghakl'
+print(sort_alphabet(string))
