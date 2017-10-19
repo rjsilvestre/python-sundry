c4e26d97fb60ae429a5c51f6ccf25819f7994801
Rafael José <rjsilvestre@gmail.com>
2017-10-19 19:52:48 +0800

    Added description and test case to polysum

--- edx-mitx-6.00.1x-polysum.py
+++ edx-mitx-6.00.1x-polysum.py
@@ -1,3 +1,11 @@
+# A regular polygon has 'n' number of sides. Each side has length 's'.
+# The area of regular polygon is: (0.25*n*s^2)/tan(pi/n)
+# The perimeter of a polygon is: length of the boundary of the polygon
+# This function called 'polysum' that takes 2 arguments, 'n' and 's'.
+# This function should sum the area and square of the perimeter of the 
+# regular polygon. The function returns the sum, rounded to 4 decimal places.
+
+
 import math
 
 def polysum(n, s):
@@ -14,3 +22,6 @@ def polysum(n, s):
     perim = n*s
     result = round(area + perim**2, 4)
     return result
+
+# Test case
+print(polysum(70, 30))
