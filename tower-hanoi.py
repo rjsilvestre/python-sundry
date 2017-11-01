fe71e68c66294cdacb354072219b1f4ed1a66a82
Rafael José <rjsilvestre@gmail.com>
2017-11-01 19:33:40 +0800

    Added tower of hanoi script

---
+++ tower-hanoi.py
@@ -0,0 +1,28 @@
+# Towers of Hanoi solution program. Uses recursive calls
+# to calculate the movements of each piece on the poles.
+# Base on the solution presented on edx mitx 6.00.1x course.
+
+
+def hanoi(n, init, temp, final):
+    '''
+    Args:
+        n: int. Number of pieces.
+        init: Initial pole name.
+        temp: Temporary pole name.
+        final: Final pole name.
+
+    Returns:
+        None.
+
+    Prints the next movement to be executed 
+    or calls recursively if needed.
+    '''
+    if n == 1:
+        print('Move from:',init, 'to', final)
+    else:
+        hanoi(n - 1, init, final, temp)
+        hanoi(1, init, temp, final)
+        hanoi(n - 1, temp, init, final)
+
+blocks = int(input('Number of blocks: '))
+hanoi(blocks, 'Pole 1', 'Pole 2', 'Pole 3')
