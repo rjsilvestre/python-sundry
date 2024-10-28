ee2481686ce9bee0b5a9c549c2f50532ffb4e3f7
Rafael José <rjsilvestre@gmail.com>
2024-10-29 01:02:52 +0800

    Detele the courses exercises. Just keep personal experiments.

--- intro-comp-prog-guttag/fex-p030-01.py
+++
@@ -1,8 +0,0 @@
-# A program that sums all the numbers contained in a string s that contains
-# a sequence of decimal numbers separated by commas.
-
-s = '1.23,4.5,6.78'
-ans = 0
-for num in s.split(','):
-    ans += float(num)
-print(ans)
--- intro-comp-prog-guttag/fex-p030-02.py
+++
@@ -1,5 +0,0 @@
-# A program that sums all the numbers contained in a string s that contains
-# a sequence of decimal numbers separated by commas.
-
-s = '1.23,4.5,6.78'
-print(sum(float(num) for num in s.split(',')))
